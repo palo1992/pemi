60f3e045385b6c0e4df0834df351404b8e9c087e
palo1992 <65390185+palo1992@users.noreply.github.com>
2021-03-20 18:41:13 +0100

    added photos

--- src/imgArray.js
+++ src/imgArray.js
@@ -7,7 +7,102 @@ const imgArray = [
     images: [
       {
         id: 1,
-        src: "/pemi/assets/gallery/plumbing/plumbing.jpg",
+        src: "/pemi/assets/gallery/plumbing/plumbing1.jpg",
+        category: "plumbing",
+      },
+      {
+        id: 2,
+        src: "/pemi/assets/gallery/plumbing/plumbing2.jpg",
+        category: "plumbing",
+      },
+      {
+        id: 3,
+        src: "/pemi/assets/gallery/plumbing/plumbing3.jpg",
+        category: "plumbing",
+      },
+      {
+        id: 4,
+        src: "/pemi/assets/gallery/plumbing/plumbing4.jpg",
+        category: "plumbing",
+      },
+      {
+        id: 5,
+        src: "/pemi/assets/gallery/plumbing/plumbing5.jpg",
+        category: "plumbing",
+      },
+      {
+        id: 6,
+        src: "/pemi/assets/gallery/plumbing/plumbing6.jpg",
+        category: "plumbing",
+      },
+      {
+        id: 7,
+        src: "/pemi/assets/gallery/plumbing/plumbing7.jpg",
+        category: "plumbing",
+      },
+      {
+        id: 8,
+        src: "/pemi/assets/gallery/plumbing/plumbing8.jpg",
+        category: "plumbing",
+      },
+      {
+        id: 9,
+        src: "/pemi/assets/gallery/plumbing/plumbing9.jpg",
+        category: "plumbing",
+      },
+      {
+        id: 10,
+        src: "/pemi/assets/gallery/plumbing/plumbing10.jpg",
+        category: "plumbing",
+      },
+      {
+        id: 11,
+        src: "/pemi/assets/gallery/plumbing/plumbing11.jpg",
+        category: "plumbing",
+      },
+      {
+        id: 12,
+        src: "/pemi/assets/gallery/plumbing/plumbing12.jpg",
+        category: "plumbing",
+      },
+      {
+        id: 13,
+        src: "/pemi/assets/gallery/plumbing/plumbing13.jpg",
+        category: "plumbing",
+      },
+      {
+        id: 14,
+        src: "/pemi/assets/gallery/plumbing/plumbing14.jpg",
+        category: "plumbing",
+      },
+      {
+        id: 15,
+        src: "/pemi/assets/gallery/plumbing/plumbing15.jpg",
+        category: "plumbing",
+      },
+      {
+        id: 16,
+        src: "/pemi/assets/gallery/plumbing/plumbing16.jpg",
+        category: "plumbing",
+      },
+      {
+        id: 17,
+        src: "/pemi/assets/gallery/plumbing/plumbing17.jpg",
+        category: "plumbing",
+      },
+      {
+        id: 18,
+        src: "/pemi/assets/gallery/plumbing/plumbing18.jpg",
+        category: "plumbing",
+      },
+      {
+        id: 19,
+        src: "/pemi/assets/gallery/plumbing/plumbing19.jpg",
+        category: "plumbing",
+      },
+      {
+        id: 20,
+        src: "/pemi/assets/gallery/plumbing/plumbing20.jpg",
         category: "plumbing",
       },
     ],
@@ -20,7 +115,117 @@ const imgArray = [
     images: [
       {
         id: 1,
-        src: "/pemi/assets/gallery/heating/radiator.jpg",
+        src: "/pemi/assets/gallery/heating/heating1.jpg",
+        category: "heating",
+      },
+      {
+        id: 2,
+        src: "/pemi/assets/gallery/heating/heating2.jpg",
+        category: "heating",
+      },
+      {
+        id: 3,
+        src: "/pemi/assets/gallery/heating/heating3.jpg",
+        category: "heating",
+      },
+      {
+        id: 4,
+        src: "/pemi/assets/gallery/heating/heating4.jpg",
+        category: "heating",
+      },
+      {
+        id: 5,
+        src: "/pemi/assets/gallery/heating/heating5.jpg",
+        category: "heating",
+      },
+      {
+        id: 6,
+        src: "/pemi/assets/gallery/heating/heating6.jpg",
+        category: "heating",
+      },
+      {
+        id: 7,
+        src: "/pemi/assets/gallery/heating/heating7.jpg",
+        category: "heating",
+      },
+      {
+        id: 8,
+        src: "/pemi/assets/gallery/heating/heating8.jpg",
+        category: "heating",
+      },
+      {
+        id: 9,
+        src: "/pemi/assets/gallery/heating/heating9.jpg",
+        category: "heating",
+      },
+      {
+        id: 10,
+        src: "/pemi/assets/gallery/heating/heating10.jpg",
+        category: "heating",
+      },
+      {
+        id: 11,
+        src: "/pemi/assets/gallery/heating/heating11.jpg",
+        category: "heating",
+      },
+      {
+        id: 12,
+        src: "/pemi/assets/gallery/heating/heating12.jpg",
+        category: "heating",
+      },
+      {
+        id: 13,
+        src: "/pemi/assets/gallery/heating/heating13.jpg",
+        category: "heating",
+      },
+      {
+        id: 14,
+        src: "/pemi/assets/gallery/heating/heating14.jpg",
+        category: "heating",
+      },
+      {
+        id: 15,
+        src: "/pemi/assets/gallery/heating/heating15.jpg",
+        category: "heating",
+      },
+      {
+        id: 16,
+        src: "/pemi/assets/gallery/heating/heating16.jpg",
+        category: "heating",
+      },
+      {
+        id: 17,
+        src: "/pemi/assets/gallery/heating/heating17.jpg",
+        category: "heating",
+      },
+      {
+        id: 18,
+        src: "/pemi/assets/gallery/heating/heating18.jpg",
+        category: "heating",
+      },
+      {
+        id: 19,
+        src: "/pemi/assets/gallery/heating/heating19.jpg",
+        category: "heating",
+      },
+      {
+        id: 20,
+        src: "/pemi/assets/gallery/heating/heating20.jpg",
+        category: "heating",
+      },
+      {
+        id: 21,
+        src: "/pemi/assets/gallery/heating/heating21.jpg",
+        category: "heating",
+      },
+      {
+        id: 22,
+        src: "/pemi/assets/gallery/heating/heating22.jpg",
+        category: "heating",
+      },
+      {
+        id: 23,
+        src: "/pemi/assets/gallery/heating/heating23.jpg",
         category: "heating",
       },
     ],
@@ -33,7 +238,37 @@ const imgArray = [
     images: [
       {
         id: 1,
-        src: "/pemi/assets/gallery/sewerage/sewerage.jpg",
+        src: "/pemi/assets/gallery/sewerage/sewerage1.jpg",
+        category: "sewerage",
+      },
+      {
+        id: 2,
+        src: "/pemi/assets/gallery/sewerage/sewerage2.jpg",
+        category: "sewerage",
+      },
+      {
+        id: 3,
+        src: "/pemi/assets/gallery/sewerage/sewerage3.jpg",
+        category: "sewerage",
+      },
+      {
+        id: 4,
+        src: "/pemi/assets/gallery/sewerage/sewerage4.jpg",
+        category: "sewerage",
+      },
+      {
+        id: 5,
+        src: "/pemi/assets/gallery/sewerage/sewerage5.jpg",
+        category: "sewerage",
+      },
+      {
+        id: 6,
+        src: "/pemi/assets/gallery/sewerage/sewerage6.jpg",
+        category: "sewerage",
+      },
+      {
+        id: 7,
+        src: "/pemi/assets/gallery/sewerage/sewerage7.jpg",
         category: "sewerage",
       },
     ],
@@ -46,7 +281,42 @@ const imgArray = [
     images: [
       {
         id: 1,
-        src: "/pemi/assets/gallery/waterwork/plumbing_pipes.jpg",
+        src: "/pemi/assets/gallery/waterwork/waterwork1.jpg",
+        category: "waterwork",
+      },
+      {
+        id: 2,
+        src: "/pemi/assets/gallery/waterwork/waterwork2.jpg",
+        category: "waterwork",
+      },
+      {
+        id: 3,
+        src: "/pemi/assets/gallery/waterwork/waterwork3.jpg",
+        category: "waterwork",
+      },
+      {
+        id: 4,
+        src: "/pemi/assets/gallery/waterwork/waterwork4.jpg",
+        category: "waterwork",
+      },
+      {
+        id: 5,
+        src: "/pemi/assets/gallery/waterwork/waterwork5.jpg",
+        category: "waterwork",
+      },
+      {
+        id: 6,
+        src: "/pemi/assets/gallery/waterwork/waterwork6.jpg",
+        category: "waterwork",
+      },
+      {
+        id: 7,
+        src: "/pemi/assets/gallery/waterwork/waterwork7.jpg",
+        category: "waterwork",
+      },
+      {
+        id: 8,
+        src: "/pemi/assets/gallery/waterwork/waterwork8.jpg",
         category: "waterwork",
       },
     ],
@@ -59,13 +329,151 @@ const imgArray = [
     images: [
       {
         id: 1,
-        src: "/pemi/assets/gallery/gas/gas.jpg",
+        src: "/pemi/assets/gallery/gas/gas1.jpg",
+        category: "gas",
+      },
+      {
+        id: 2,
+        src: "/pemi/assets/gallery/gas/gas2.jpg",
+        category: "gas",
+      },
+      {
+        id: 3,
+        src: "/pemi/assets/gallery/gas/gas3.jpg",
+        category: "gas",
+      },
+      {
+        id: 4,
+        src: "/pemi/assets/gallery/gas/gas4.jpg",
+        category: "gas",
+      },
+      {
+        id: 5,
+        src: "/pemi/assets/gallery/gas/gas5.jpg",
+        category: "gas",
+      },
+      {
+        id: 6,
+        src: "/pemi/assets/gallery/gas/gas6.jpg",
+        category: "gas",
+      },
+      {
+        id: 7,
+        src: "/pemi/assets/gallery/gas/gas7.jpg",
+        category: "gas",
+      },
+      {
+        id: 8,
+        src: "/pemi/assets/gallery/gas/gas8.jpg",
+        category: "gas",
+      },
+      {
+        id: 9,
+        src: "/pemi/assets/gallery/gas/gas9.jpg",
         category: "gas",
       },
     ],
   },
   {
     id: 6,
+    src: "/pemi/assets/gallery/source/source6.jpg",
+    title: "kotly",
+    category: "source",
+    images: [
+      {
+        id: 1,
+        src: "/pemi/assets/gallery/source/source1.jpg",
+        category: "source",
+      },
+      {
+        id: 2,
+        src: "/pemi/assets/gallery/source/source2.jpg",
+        category: "source",
+      },
+      {
+        id: 3,
+        src: "/pemi/assets/gallery/source/source3.jpg",
+        category: "source",
+      },
+      {
+        id: 4,
+        src: "/pemi/assets/gallery/source/source4.jpg",
+        category: "source",
+      },
+      {
+        id: 5,
+        src: "/pemi/assets/gallery/source/source5.jpg",
+        category: "source",
+      },
+      {
+        id: 6,
+        src: "/pemi/assets/gallery/source/source6.jpg",
+        category: "source",
+      },
+      {
+        id: 7,
+        src: "/pemi/assets/gallery/source/source7.jpg",
+        category: "source",
+      },
+      {
+        id: 8,
+        src: "/pemi/assets/gallery/source/source8.jpg",
+        category: "source",
+      },
+      {
+        id: 9,
+        src: "/pemi/assets/gallery/source/source9.jpg",
+        category: "source",
+      },
+      {
+        id: 10,
+        src: "/pemi/assets/gallery/source/source10.jpg",
+        category: "source",
+      },
+      {
+        id: 11,
+        src: "/pemi/assets/gallery/source/source11.jpg",
+        category: "source",
+      },
+      {
+        id: 12,
+        src: "/pemi/assets/gallery/source/source12.jpg",
+        category: "source",
+      },
+      {
+        id: 13,
+        src: "/pemi/assets/gallery/source/source13.jpg",
+        category: "source",
+      },
+      {
+        id: 14,
+        src: "/pemi/assets/gallery/source/source14.jpg",
+        category: "source",
+      },
+      {
+        id: 15,
+        src: "/pemi/assets/gallery/source/source15.jpg",
+        category: "source",
+      },
+      {
+        id: 16,
+        src: "/pemi/assets/gallery/source/source16.jpg",
+        category: "source",
+      },
+      {
+        id: 17,
+        src: "/pemi/assets/gallery/source/source17.jpg",
+        category: "source",
+      },
+      {
+        id: 18,
+        src: "/pemi/assets/gallery/source/source18.jpg",
+        category: "source",
+      },
+    ],
+  },
+  {
+    id: 7,
     src: "/pemi/assets/gallery/car_Service.jpg",
     title: "servis automobilov",
     category: "car",
